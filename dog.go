b5b00b3eda66de26a8d94d179915558a57638213
MrMiyag1 <adedayo.adepoju1@gmail.com>
2020-09-24 14:01:26 -0700

    Create dog.go
    Package dog allows us to better understand the evolution of the canine lifes

---
+++ dog.go
@@ -0,0 +1,7 @@
+// package dog takes in a human year and converts it into dog years.
+package dog
+
+// Years converts human year to dog year by multiplying by 7
+func Years(hy int) int {
+	return hy * 7
+}
